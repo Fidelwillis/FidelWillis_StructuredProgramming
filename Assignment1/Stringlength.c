f5625fd2603c957a06784fc1a34a17d41ce17d72
Fidelwillis <fidel.odiwuor@students.jkuat.ac.ke>
2025-11-17 23:15:27 +0300

    Update Stringlength.c

--- Assignment1/Stringlength.c
+++ Assignment1/Stringlength.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100]; 
+
+    // Ask the user for their name
+    printf("Enter your name: ");
+    scanf("%s", &name); 
+
+    
+    printf("You entered: %s\n", name);
+
+    // Find and display the length
+    printf("Length of the string: %lu\n", strlen(name));
+
+    return 0;
+}

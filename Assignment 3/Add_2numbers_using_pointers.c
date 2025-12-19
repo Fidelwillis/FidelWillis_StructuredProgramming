cca4f26b14f8bf98d53d1b1b6b3467c70901f172
Fidelwillis <fidel.odiwuor@students.jkuat.ac.ke>
2025-12-19 10:40:51 +0300

    Update Add_2numbers_using_pointers.c

--- Assignment 3/Add_2numbers_using_pointers.c
+++ Assignment 3/Add_2numbers_using_pointers.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main() {
+    int number1 = 3;
+    int number2 = 7;
+    int sum;
+
+    int *ptr1;
+    int *ptr2;
+
+    ptr1 = &number1; 
+    ptr2 = &number1;    
+
+    sum = *ptr1 + *ptr2;  
+
+    printf("Sum of numbers: %d\n", sum);
+
+    return 0;
+}

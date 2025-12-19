dcf2b8081e7398b3083e5aa945d4d789b7dba80c
Fidelwillis <fidel.odiwuor@students.jkuat.ac.ke>
2025-12-19 10:41:33 +0300

    Update Display_address_of_variable.c

--- Assignment 3/Display_address_of_variable.c
+++ Assignment 3/Display_address_of_variable.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+
+int main() {
+    int number = 10;     
+    int *ptr;          
+    ptr = &number;        
+
+    printf("Value of number: %d\n", number);
+    printf("Value stored in ptr (address of number): %p\n", ptr);
+    printf("Address of number: %p\n", &number);
+    printf("Value accessed using *ptr: %d\n", *ptr);
+
+    return 0;
+}

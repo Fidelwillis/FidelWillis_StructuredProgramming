29602b1023b9be6ba40ed9a44e2476551f706a09
Fidelwillis <fidel.odiwuor@students.jkuat.ac.ke>
2025-12-19 10:43:27 +0300

    Update swapping2numbers_using pointers.c

--- Assignment 3/swapping2numbers_using pointers.c
+++ Assignment 3/swapping2numbers_using pointers.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+void swapNumbers(int *x, int *y) {
+    int age;
+
+    age = *x;
+    *x = *y;
+    *y = age;
+}
+
+int main() {
+    int p = 5;
+    int n = 10;
+
+    printf("Before swapping:\n");
+    printf("a = %d, b = %d\n", p, n);
+
+    swapNumbers(&p, &n);
+
+    printf("After swapping:\n");
+    printf("a = %d, b = %d\n", p, n);
+
+    return 0;
+}

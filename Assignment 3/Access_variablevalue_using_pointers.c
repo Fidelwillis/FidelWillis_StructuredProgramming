341e66341178c16235a2302165507709b46d320e
Fidelwillis <fidel.odiwuor@students.jkuat.ac.ke>
2025-12-19 10:40:15 +0300

    Update Access_variablevalue_using_pointers.c

--- Assignment 3/Access_variablevalue_using_pointers.c
+++ Assignment 3/Access_variablevalue_using_pointers.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+
+int main() {
+    int count = 10;        
+    int *pCount;        
+
+    pCount = &count;      
+
+    *pCount = 20;          
+
+    printf("Updated value: %d\n", count);
+
+    return 0;
+}
